c1428a6807c139d7a3e82a0dded909ec2b9b8fa6
Minsu <jminsu084@gmail.com>
2024-05-26 22:33:39 +0900

    feat : 회사 이름, 기술 스택, 지역 데이터를 JSON 또는 Excel 파일로 저장하는 함수 구현 완료

--- employmentInfo/jumpit_crawler.py
+++ employmentInfo/jumpit_crawler.py
@@ -1,14 +1,36 @@
 import requests  # htlm 코드를 가져오기 위한 모듈
 import json  # json import하기
+import pandas as pd
 
 
-def save_file():
+def save_to_excel(data, file_path):
+    """
+    데이터를 엑셀 파일로 저장하는 함수
+
+    :param data: dict, 저장할 딕셔너리 (회사 이름, 기술 스택, 지역)
+    :param file_path: str, 엑셀 데이터를 저장할 파일 경로
+    :return: None
+    """
+    df = pd.DataFrame(data)
+    df.to_excel(file_path, index=False)
+    print(f"Data successfully saved to {file_path}")
+
+
+def save_file_json(data, file_path):
+    """
+    데이터를 JSON 파일로 저장하는 함수
+    
+    :param data: dict, 저장할 딕셔너리 (회사 이름, 기술 스택, 지역)
+    :param file_path: str, 파일 이름
+    :return: None
     """
 
-    :return:
-    """
-
-    pass
+    try:
+        with open(file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+        print(f"Data successfully saved to {file_path}")
+    except Exception as e:
+        print(f"An error occurred while saving the file: {e}")
 
 
 def crawler():
@@ -57,7 +79,8 @@ def crawler():
                 print(skill)
                 temp_skill.append(skill)  # 기술 임시 저장
 
-            temp_all['skill'] = temp_skill
+            # temp_all['skill'] = temp_skill  # 기술 스택을 리스트로 저장하길 원할 때 사용
+            temp_all['skill'] = ', '.join(temp_skill)  # 기술 스택을 문자열로 변환하여 저장 
 
             tech_info.append(temp_all)
 
@@ -70,3 +93,6 @@ if __name__ == "__main__":
     tech_info = crawler()
     print("\n\ncrawler() 종료\n\n")
     print(tech_info)
+
+    save_file_json(tech_info, "techInfoData/tech_info.json")
+    save_to_excel(tech_info, "techInfoData/tech_info.xlsx")
